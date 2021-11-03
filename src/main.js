40feb14ee76aa8763d28918272be40b831f9c7e9
rvladimirvm <ramvlay@gmail.com>
2021-11-02 23:50:43 -0500

    Unused files was removed

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
 const swaggerDocument = YAML.load('./src/swagger/swagger.yaml')
 
-
 app.use(morgan('dev'))
 
 app.use(cors({
